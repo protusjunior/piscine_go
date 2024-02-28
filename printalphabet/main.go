26eae7e6adab61b438fa765cdaa34d1e24d0d8b6
pjunior <juniorprptus75@gmail.com>
2024-02-28 20:16:59 +0300

    main.go

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -2,9 +2,10 @@ package main
 
 import "github.com/01-edu/z01"
 
-func main(){
+func main() {
 	for i := 97; i <= 122; i++ {
 		z01.PrintRune(rune(i))
 	}
-z01.PrintRune('\n')
+	z01.PrintRune('\n')
+
 }
