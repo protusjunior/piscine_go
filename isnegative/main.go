c39a6c9a77e4400caffb839eb3608868c2888eee
Protus Junior <pjunior@noreply.learn.zone01kisumu.ke>
2024-03-01 17:34:00 +0000

    isnegative/main.go

--- isnegative/main.go
+++ isnegative/main.go
@@ -1,5 +1,13 @@
 package piscine
 
-import () {
-	
+import "github.com/01-edu/z01"
+
+func Isnegative(nb int) {
+	if nb >= 0 {
+		z01.PrintRune('F')
+		z01.PrintRune('\n')
+	} else {
+		z01.PrintRune('T')
+		z01.PrintRune('\n')
+	}
 }
