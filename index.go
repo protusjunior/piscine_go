a3c69c79dd5c4cdf6afe4d75b72c3326cae8b153
Protus Junior <pjunior@noreply.learn.zone01kisumu.ke>
2024-03-07 16:20:12 +0000

    Update 'index.go'

--- index.go
+++ index.go
@@ -4,7 +4,7 @@ func Index(s string, toFind string) int {
 	for i := 0; i <= len(s)-len(toFind); i++ {
 		k := s[i : i+len(toFind)]
 		if k == toFind {
-			    return i
+			return i
 		}
 	}
 	return -1
