92a4a0e7473ff74b7b218c2c339b15e369e2792a
Protus Junior <pjunior@noreply.learn.zone01kisumu.ke>
2024-03-12 17:07:56 +0000

    Upload files to ''

--- main.go
+++ main.go
@@ -1,36 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"os"
-
-	"github.com/01-edu/z01"
 )
 
-func printStr(s string) {
-	for _, r := range s {
-		z01.PrintRune(r)
-	}
-	z01.PrintRune('\n')
-}
-
-func isEven(nbr int) bool {
-	if nbr == 1 {
-		return false
-	}
-	if nbr%2 != 0 {
-		return false
-	} else {
-		return true
-	}
-}
-
 func main() {
-	lengthOfArg := len(os.Args[1:])
-	EvenMsg := "I have an even number of arguments"
-	OddMsg := "I have an odd number of arguments"
-	if isEven(lengthOfArg) {
-		printStr(EvenMsg)
-	} else {
-		printStr(OddMsg)
+	args := os.Args[1:]
+	if len(args) > 1 {
+		fmt.Println("Too many arguments")
+		return
 	}
+	if len(args) < 1 {
+		fmt.Println("File name missing")
+		return
+	}
+	data, err := os.ReadFile(args[0])
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+	fmt.Print(string(data))
 }
