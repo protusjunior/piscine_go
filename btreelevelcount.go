e270156492d0ba61bddab3550851453f55f97ede
pjunior <juniorprotus75@gmail.com>
2024-03-19 23:12:58 +0300

    btreelevelcount.go

--- btreelevelcount.go
+++ btreelevelcount.go
@@ -4,16 +4,8 @@ func BTreeLevelCount(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-
-	Left := BTreeLevelCount(root.Left)
-	Right := BTreeLevelCount(root.Right)
-
-	return Max(Left, Right) + 1
-}
-
-func Max(a, b int) int {
-	if a > b {
-		return a
+	if BTreeLevelCount(root.Left) > BTreeLevelCount(root.Right) {
+		return BTreeLevelCount(root.Left) + 1
 	}
-	return b
+	return BTreeLevelCount(root.Right) + 1
 }
