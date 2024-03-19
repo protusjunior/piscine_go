efc36372371103ca468201c1705330ecb66fd45c
Christopher Fremond <dev@01-edu.org>
2024-03-19 20:48:42 +0000

    Update 'listremoveif.go'

--- listremoveif.go
+++ listremoveif.go
@@ -1,22 +1,18 @@
 package piscine
 
 func ListRemoveIf(l *List, data_ref interface{}) {
-	prev := &NodeL{}
-	prev = nil
-	h := l.Head
-	for h != nil {
-		if data_ref == h.Data {
-			if prev == nil {
-				l.Head = l.Head.Next
-				// h = h.Next
-			} else {
-				prev.Next = h.Next
-				h = prev
+	current := l.Head
+	var prev *NodeL
 
-			}
+	for current != nil && current.Data == data_ref {
+		l.Head = current.Next
+		current = l.Head
+	}
+
+	for current != nil {
+		if current.Data == data_ref {
+			prev.Next = current.Next
 		} else {
-			prev = h
-		}
-		h = h.Next
+			prrent.Next
 	}
 }
