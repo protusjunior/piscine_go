8f2c9a568556c40fbe571dafb5a7974d8e0a2c99
Christopher Fremond <dev@01-edu.org>
2024-03-19 22:45:00 +0000

    Update 'listremoveif.go'

--- listremoveif.go
+++ listremoveif.go
@@ -1,5 +1,16 @@
 package piscine
 
+func ListPushBack(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = n
+		l.Tail = n
+	} else {
+		l.Tail.Next = n
+		l.Tail = n
+	}
+}
+
 func ListRemoveIf(l *List, data_ref interface{}) {
 	current := l.Head
 	var prev *NodeL
